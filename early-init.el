82b695cdbc4e042e1feaeb9a836c35a7820d7291
Joe Sadusk <joe.sadusk@prometheus.co>
2026-04-13 23:37:16 -0700

    Updates for work

---
+++ early-init.el
@@ -0,0 +1,7 @@
+;-*-  mode: lisp; lexical-binding: t;  -*-
+(setq package-enable-at-startup nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; no-native-compile: t
+;; no-update-autoloads: t
+;; End:
